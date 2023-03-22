5d2f20d5d91ffd426a56c56e468560328863f009
Mike Audi <mike@mytiki.com>
2023-03-21 22:35:52 -0500

    add prettier

--- src/b64.js
+++ src/b64.js
@@ -3,29 +3,25 @@
  * MIT license. See LICENSE file in root directory.
  */
 
-export { decode, encode }
+export { decode, encode };
 
-function decode (string, isUrlEncoded) {
-  const m = string.length % 4
-  let input = string
+function decode(string, isUrlEncoded) {
+  const m = string.length % 4;
+  let input = string;
   if (isUrlEncoded) {
-    input = input
-      .replace(/-/g, '+')
-      .replace(/_/g, '/')
+    input = input.replace(/-/g, "+").replace(/_/g, "/");
   }
-  input.padEnd(string.length + (m === 0 ? 0 : 4 - m), '=')
-  return Uint8Array.from(atob(input), c => c.charCodeAt(0)).buffer
+  input.padEnd(string.length + (m === 0 ? 0 : 4 - m), "=");
+  return Uint8Array.from(atob(input), (c) => c.charCodeAt(0)).buffer;
 }
 
-function encode (bytes, urlEncode, withPad) {
-  let res = btoa(String.fromCharCode.apply(null, bytes))
+function encode(bytes, urlEncode, withPad) {
+  let res = btoa(String.fromCharCode.apply(null, bytes));
   if (urlEncode) {
-    res = res
-      .replace(/\+/g, '-')
-      .replace(/\//g, '_')
+    res = res.replace(/\+/g, "-").replace(/\//g, "_");
   }
   if (!withPad) {
-    res = res.replace(/=+$/, '')
+    res = res.replace(/=+$/, "");
   }
-  return res
+  return res;
 }
--- src/l0Storage.js
+++ src/l0Storage.js
@@ -3,48 +3,63 @@
  * MIT license. See LICENSE file in root directory.
  */
 
-import * as b64 from './b64.js'
+import * as b64 from "./b64.js";
 
-export { report, decode, guardClaims }
+export { report, decode, guardClaims };
 
-async function report (url, key, body) {
+async function report(url, key, body) {
   return fetch(url, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      Authorization: 'Basic ' + btoa(`${key.id}:${key.secret}`)
+      "Content-Type": "application/json",
+      Authorization: "Basic " + btoa(`${key.id}:${key.secret}`),
     },
-    body: JSON.stringify(body)
-  })
+    body: JSON.stringify(body),
+  });
 }
 
-async function decode (jwt, pubKey, jwtAlg) {
-  const split = jwt.split('.')
-  const headerB64 = split[0]
-  const payloadB64 = split[1]
-  const signatureB64 = split[2]
+async function decode(jwt, pubKey, jwtAlg) {
+  const split = jwt.split(".");
+  const headerB64 = split[0];
+  const payloadB64 = split[1];
+  const signatureB64 = split[2];
 
   const cryptoKey = await crypto.subtle.importKey(
-    'jwk', pubKey, jwtAlg, false, ['verify'])
+    "jwk",
+    pubKey,
+    jwtAlg,
+    false,
+    ["verify"]
+  );
 
-  const isValid = await crypto.subtle.verify(jwtAlg, cryptoKey,
-    b64.decode(signatureB64, true), new TextEncoder().encode([headerB64, payloadB64].join('.')))
+  const isValid = await crypto.subtle.verify(
+    jwtAlg,
+    cryptoKey,
+    b64.decode(signatureB64, true),
+    new TextEncoder().encode([headerB64, payloadB64].join("."))
+  );
 
-  if (!isValid) { throw new Error('Failed to validate JWT') }
-  return JSON.parse(new TextDecoder().decode(b64.decode(payloadB64, true)))
+  if (!isValid) {
+    throw new Error("Failed to validate JWT");
+  }
+  return JSON.parse(new TextDecoder().decode(b64.decode(payloadB64, true)));
 }
 
-function guardClaims (req, config) {
-  const reqClaims = config.claims.split(',')
+function guardClaims(req, config) {
+  const reqClaims = config.claims.split(",");
   reqClaims.forEach((claim, i) => {
-    if (req[claim] == null) { throw new Error('Missing required claim: ' + claim) }
-  })
+    if (req[claim] == null) {
+      throw new Error("Missing required claim: " + claim);
+    }
+  });
 
-  if (req.iss !== config.iss) { throw new Error('Invalid ISS claim') }
+  if (req.iss !== config.iss) {
+    throw new Error("Invalid ISS claim");
+  }
 
-  const iatDate = new Date(req.iat * 1000)
-  const expDate = new Date((req.exp * 1000) + (config.clockSkew * 60 * 1000))
+  const iatDate = new Date(req.iat * 1000);
+  const expDate = new Date(req.exp * 1000 + config.clockSkew * 60 * 1000);
 
-  if (iatDate >= new Date()) throw new Error('Invalid IAT claim')
-  if (expDate < new Date()) throw new Error('Invalid EXP claim')
+  if (iatDate >= new Date()) throw new Error("Invalid IAT claim");
+  if (expDate < new Date()) throw new Error("Invalid EXP claim");
 }
