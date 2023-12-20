d83ff06d4af40d02888944d09b3f1ad0ecebc12d
Mike Audi <mike@mytiki.com>
2023-12-20 13:43:59 -0600

    feat!: no more typescript

--- src/l0/storage/bucket/bucket-req.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) TIKI Inc.
- * MIT license. See LICENSE file in root directory.
- */
-
-interface BucketReq {
-  key: string;
-  file: Uint8Array;
-}
--- src/l0/storage/l0-storage-req.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) TIKI Inc.
- * MIT license. See LICENSE file in root directory.
- */
-
-interface L0StorageReq {
-  path: string;
-  sizeBytes: number;
-}
